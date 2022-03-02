753262f26a44bc2bed5dc4263be6e313ae63546a
ayseleynavuz <ayseleynavuz@gmail.com>
2022-03-02 19:57:31 +0300

    Yazar Ekleme İşlemi Yapıldı

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını adını boş geçemezsiniz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazarın hakkımda kısmını boş geçemezsiniz.");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen en az iki karakter girişi yapınız.");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen elli karakterden fazla değer girişi yapmayınız.");
+        }
+    }
+}
--- WebApplication/Controllers/WriterController.cs
+++ WebApplication/Controllers/WriterController.cs
@@ -1,5 +1,9 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.concrete;
+using FluentValidation;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +21,30 @@ namespace WebApplication.Controllers
             var WriterValues = wm.Getlist();
             return View(WriterValues);
 
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid) //eğer sonuçlar geçerliyse
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
