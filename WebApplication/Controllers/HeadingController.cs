ac61740af88f4c056f63a07773b219054ea40dec
ayseleynavuz <ayseleynavuz@gmail.com>
2022-03-04 16:23:16 +0300

    Başlık düzenlemeleri yapıldı

---
+++ WebApplication/Controllers/HeadingController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = headingManager.GetList();
+            return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString(),
+
+                                                  }).ToList();
+            List<SelectListItem> valueWriter = (from x in writerManager.Getlist()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurname,
+                                                    Value = x.WriterId.ToString()
+
+                                                }).ToList();
+
+            ViewBag.vlc = valueCategory;
+            ViewBag.vlw = valueWriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString()); //gönderilen parametreye bugünün kısa tarihini eklemiş olacak
+            headingManager.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
